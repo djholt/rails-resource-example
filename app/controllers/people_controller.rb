20950cf1719183dbed62bf649c835e65d0bce134
DJ Holt <dj@holt.dj>
2014-07-10 10:03:28 -0600

    Finish people controller/views

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,5 +1,9 @@
 class PeopleController < ApplicationController
 
+  def index
+    @people = Person.all
+  end
+
   def show
     @person = Person.find(params[:id])
   end
@@ -12,6 +16,22 @@ class PeopleController < ApplicationController
     @person = Person.new(person_params)
     @person.save
     redirect_to @person
+  end
+
+  def edit
+    @person = Person.find(params[:id])
+  end
+
+  def update
+    @person = Person.find(params[:id])
+    @person.update_attributes(person_params)
+    redirect_to @person
+  end
+
+  def destroy
+    @person = Person.find(params[:id])
+    @person.destroy
+    redirect_to people_path
   end
 
   private
