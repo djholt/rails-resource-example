11af59c94f8a6bfa9f1eea4867b9138e3812a609
DJ Holt <dj@holt.dj>
2014-07-11 10:08:38 -0600

    Add validations, respond_with

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,37 +1,60 @@
 class PeopleController < ApplicationController
 
+  respond_to :html, :json, :xml
+
   def index
     @people = Person.all
+    respond_with @people
   end
 
   def show
     @person = Person.find(params[:id])
+    respond_with @person
   end
 
   def new
     @person = Person.new
+    respond_with @person
   end
 
   def create
     @person = Person.new(person_params)
     @person.save
-    redirect_to @person
+    respond_with @person
+
+    # alternative method (old style):
+    # if @person.save
+    #   redirect_to @person
+    # else
+    #  render :new
+    # end
   end
 
   def edit
     @person = Person.find(params[:id])
+    respond_with @person
   end
 
   def update
     @person = Person.find(params[:id])
     @person.update_attributes(person_params)
-    redirect_to @person
+    respond_with @person
+
+    # alternative method (old style):
+    # if @person.update_attributes(person_params)
+    #   redirect_to @person
+    # else
+    #   render :edit
+    # end
   end
 
   def destroy
     @person = Person.find(params[:id])
     @person.destroy
-    redirect_to people_path
+    respond_with @person
+
+    # alternative method (old style):
+    # redirect_to people_path
   end
 
   private
--- app/models/person.rb
+++ app/models/person.rb
@@ -1,2 +1,4 @@
 class Person < ActiveRecord::Base
+  validates :name, presence: true
+  validates :age, numericality: { only_integer: true, greater_than_or_equal_to: 13 }
 end
